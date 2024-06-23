8bcb775056b803548fae2a67d3f2d78aa44305bb
nicolasduernbeck <nicolas.duernbeck@gmx.at>
2024-06-23 12:05:46 +0200

    added form submit handler

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
 
 export default function InputForm() {
+  function handleFormSubmit(e) {
+    e.preventDefault();
+
+    //Guard clause to catch empty todos
+    if (!value.length) return;
+  }
+
   const [value, setValue] = useState('');
 
   return (
-    <form className="input__form">
+    <form className="input__form" onSubmit={handleFormSubmit}>
       <input type="text" value={value} onChange={e => setValue(e.target.value)}></input>
       <button>Add</button>
     </form>

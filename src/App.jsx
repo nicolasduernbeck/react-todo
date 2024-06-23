f9453141bf2d246ce2749d5f508bdbec8c7af556
nicolasduernbeck <nicolas.duernbeck@gmx.at>
2024-06-23 12:00:47 +0200

    initial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import InputForm from './components/InputForm';
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>Todo</h1>
+      <InputForm />
+    </div>
+  );
+}
---
+++ src/components/InputForm.jsx
@@ -0,0 +1,8 @@
+export default function InputForm() {
+  return (
+    <div className="input__form">
+      <input type="text"></input>
+      <button>Add</button>
+    </div>
+  );
+}
